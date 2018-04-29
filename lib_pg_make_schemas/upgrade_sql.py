b7a344b8a286c9c0cf4759b349e12d8bb8181221
Andrei Antonov <polymorphm@gmail.com>
2018-04-30 01:51:02 +0300

    upgrade_sql: bugfix: if ``migrations_descr`` is not exists we also need to raise exception ``missing migration``
    (cherry picked from commit 1cc5d9329f457ce13b17eb13b3d0164c08e88c8d)

--- lib_pg_make_schemas/upgrade_sql.py
+++ lib_pg_make_schemas/upgrade_sql.py
@@ -2,26 +2,23 @@
 
 def read_upgrade_sql(cluster_descr, host_type, migr):
     migrations_descr = cluster_descr.migrations
-    
-    if migrations_descr is None:
-        return
-    
     migration_descr = None
     
-    for other_migration_descr in migrations_descr.migration_list:
-        if other_migration_descr.revision != migr[0] or \
-                migr[1] not in other_migration_descr.compatible_list:
-            continue
-        
-        if migration_descr is not None:
-            raise ValueError(
-                '{!r}, {!r}: non unique migration'.format(
-                    cluster_descr.cluster_file_path,
-                    migr,
-                ),
-            )
-        
-        migration_descr = other_migration_descr
+    if migrations_descr is not None:
+        for other_migration_descr in migrations_descr.migration_list:
+            if other_migration_descr.revision != migr[0] or \
+                    migr[1] not in other_migration_descr.compatible_list:
+                continue
+            
+            if migration_descr is not None:
+                raise ValueError(
+                    '{!r}, {!r}: non unique migration'.format(
+                        cluster_descr.cluster_file_path,
+                        migr,
+                    ),
+                )
+            
+            migration_descr = other_migration_descr
     
     if migration_descr is None:
         raise ValueError(
