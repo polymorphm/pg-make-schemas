18a5d1072aa558aa179b988e239a3354dc1c4ee0
Andrei Antonov (ppdp.work) <antonov@ppdp.ru>
2026-02-12 02:25:41 +0300

    EXAMPLE.comment.sh: via git describe and a file as fallback

--- EXAMPLE.comment.sh
+++ EXAMPLE.comment.sh
@@ -4,20 +4,8 @@ set -e
 
 cd -- "${0%/*}"
 
-git --version >/dev/null
-
-rev="$(git rev-parse --verify HEAD 2>/dev/null || true)"
-
-if [ "x$rev" = "x" ]
-then
-  echo "{no-git-rev}"
-
-  exit
-fi
-
-tag="$(git tag --points-at HEAD 2>/dev/null || true)"
-dirty="$(git status -s 2>/dev/null || true)"
-
-echo "${tag:+${tag//$'\n'/+}+}$rev${dirty:+"{dirty}"}"
+git describe --long --always --dirt 2>/dev/null ||
+    cat git-descibe.txt 2>/dev/null ||
+    echo "{no-git-rev}"
 
 # vi:ts=2:sw=2:et
