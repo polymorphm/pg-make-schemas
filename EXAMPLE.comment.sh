e1384c44a95633cf429b1ecf7d5c3de26ad2ff05
Andrei Antonov <polymorphm.mobile@gmail.com>
2026-05-03 17:53:35 +0300

    Fix comment script example

--- EXAMPLE.comment.sh
+++ EXAMPLE.comment.sh
@@ -4,8 +4,8 @@ set -e
 
 cd -- "${0%/*}"
 
-git describe --long --always --dirt 2>/dev/null ||
-    cat git-descibe.txt 2>/dev/null ||
+git describe --dirty --long --always 2>/dev/null ||
+    cat git-describe.txt 2>/dev/null ||
     echo "{no-git-rev}"
 
 # vi:ts=2:sw=2:et
